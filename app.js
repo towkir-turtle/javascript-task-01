4a896a904d52a3dce6f2899988b253605e7e9b5d
Towkir Ahmed <towkir_ahmed@yahoo.com>
2022-02-24 22:15:21 +0600

    completed first task

---
+++ app.js
@@ -0,0 +1,23 @@
+//Problem-01:
+function sumMachine(num1, num2) {
+  const sum = num1 + num2;
+  return sum;
+}
+const addiction1 = sumMachine(3, 2);
+console.log(addiction1);
+const addiction2 = sumMachine(-3, -6);
+console.log(addiction2);
+const addiction3 = sumMachine(7, 3);
+console.log(addiction3);
+
+//Problem-02:
+function minuteToSecond(minute) {
+  const second = minute * 60;
+  return second;
+}
+const convert1 = minuteToSecond(300);
+console.log(convert1);
+const convert2 = minuteToSecond(180);
+console.log(convert2);
+const convert3 = minuteToSecond(120);
+console.log(convert3);
